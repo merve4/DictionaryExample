75b6e8fb3f021cc7866734cc4b24677d8699706e
merve <merveerdagi30@gmail.com>
2023-03-01 14:44:32 +0300

    Proje dosyası ekle.

---
+++ DictionaryExample/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+    class Dictionary<T1, T2> //generic class
+    {
+        KeyValuePair<T1, T2>[] items;
+
+
+        public Dictionary()  //constructor
+        {
+            items = new KeyValuePair<T1, T2>[0];
+        }
+        public void Add(T1 item1, T2 item2)
+        {
+            KeyValuePair<T1, T2>[] tempArray = items; //gecici dizi önceki değerleri saklar
+            items = new KeyValuePair<T1, T2>[items.Length + 1]; //dizinin eleman sayısını 1 arttırır.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i]; //items eski değerlerini almış oldu
+            }
+            items[items.Length - 1] = new KeyValuePair<T1, T2>(item1, item2);
+        }
+        public int Count
+        {
+            get { return items.Length; }
+        }
+
+    }
+
+
+}
+}
---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+myDictionary.Add("Assembly", 1949);
+myDictionary.Add("Basic", 1964);
+myDictionary.Add("C", 1972);
+myDictionary.Add("Python", 1991);
+myDictionary.Add("Java", 1995);
+myDictionary.Add("C#", 2000);
+
+Console.WriteLine("Eleman Sayısı : " + myDictionary.Count);
+
+foreach (var item in myDictionary)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("-----------------------");
+myDictionary.Remove("C");
+myDictionary.Remove("Python");
+foreach (var item in myDictionary)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("Silindikten sonraki eleman sayısı: " + myDictionary.Count);
+
+
